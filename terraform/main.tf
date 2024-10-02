6b4b7899ddd523a91a3c7431731499ad265a8517
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2024-10-02 16:47:16 -0400

    chore: Refactor the Terraform module to follow spec CC006

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,13 +3,17 @@
 
 resource "juju_application" "upf" {
   name  = var.app_name
-  model = var.model_name
+  model = var.model
 
   charm {
-    name    = "sdcore-upf-k8s"
-    channel = var.channel
+    name     = "sdcore-upf-k8s"
+    channel  = var.channel
+    revision = var.revision
   }
-  config = var.config
-  units  = 1
-  trust  = true
+
+  config      = var.config
+  constraints = var.constraints
+  units       = var.units
+  resources   = var.resources
+  trust       = true
 }
